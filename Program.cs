4187197478c1908f95cdf0227db9f315d32574fb
Bobby Galli <bobbyg603@gmail.com>
2022-08-05 13:58:22 -0400

    chore: add configs to support BugSplatNative

--- Program.cs
+++ Program.cs
@@ -19,6 +19,14 @@ namespace BugSplatCrashHandler
 
             [Option('z', "zipFile", Required = false, HelpText = "Zipped crash report to upload.")]
             public bool ZipFile { get; set; }
+
+            // TODO DP handle 'l' (MDSF_LOGFILE)
+            [Option('l', "logToFile", Required = false, HelpText = "Write BugSplatCrashHandler log statements to a file.")]
+            public bool LogToFile { get; set; }
+
+            // TODO DP handle 'c' (MDSF_LOGCONSOLE)
+            [Option('c', "logToConsole", Required = false, HelpText = "Write BugSplatCrashHandler log statements to standard output.")]
+            public bool LogToConsole { get; set; }
         }
 
         static void RunOptions(Options opts)
